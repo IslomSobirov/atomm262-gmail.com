486775a9b1f5a6f1dfff12b6369571545ac8b544
IslomSobirov <atomm262@gmail.com>
2020-04-05 17:39:09 +0500

    errors are executed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
     <div class="row mt-4">
         <div class="col-xl-8 col-lg-8 col-md-8 col-sm-12 col-xs-12">
             @foreach ($posts as $post)
-                <a href="post/{{$post->id}}" class="unstyled-link">
+                <a href="/post/{{$post->id}}" class="unstyled-link">
                     <div class="publication-block my-2">
                         <h5 class="publication-block-headline">{{$post->name}}</h5>
                         @if(isset($post->user->name))
